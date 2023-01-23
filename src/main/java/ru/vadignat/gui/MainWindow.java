15b4f104bbc6954b9e1d264dcffa11e677c37d10
vadignat <vadimignat02@gmail.com>
2023-01-23 09:57:10 +0300

    Добавлены класс CartesianPainter, панелька и radiobutton

--- src/main/java/ru/vadignat/gui/MainWindow.java
+++ src/main/java/ru/vadignat/gui/MainWindow.java
@@ -1,7 +1,11 @@
 package ru.vadignat.gui;
 
+import ru.vadignat.GraphicsPanel;
+import ru.vadignat.Painter;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class MainWindow extends JFrame {
     private final Dimension minSz = new Dimension(600, 400);
@@ -13,6 +17,29 @@ public class MainWindow extends JFrame {
         GroupLayout gl = new GroupLayout(getContentPane());
         setLayout(gl);
 
+        CartesianPainter crt = new CartesianPainter();
+        ArrayList<ru.vadignat.Painter> painters = new ArrayList<Painter>();
+        painters.add(crt);
 
+        GraphicsPanel mainPanel = new GraphicsPanel(painters);
+        mainPanel.setBackground(Color.WHITE);
+        JRadioButton radioButton = new JRadioButton();
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addGap(std)
+                .addGroup(gl.createParallelGroup()
+                        .addComponent(mainPanel)
+                        .addComponent(radioButton)
+                )
+                .addGap(std)
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addGap(10)
+                .addComponent(mainPanel)
+                .addGap(7)
+                .addComponent(radioButton)
+                .addGap(10)
+        );
     }
 }

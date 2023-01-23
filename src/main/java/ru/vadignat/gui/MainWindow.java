c61a081a2669eeada281143e66432b02373c1bf9
vadignat <vadimignat02@gmail.com>
2023-01-23 09:47:20 +0300

    Добавлены некоторые классы из работы в течение семестра

--- src/main/java/ru/vadignat/gui/MainWindow.java
+++ src/main/java/ru/vadignat/gui/MainWindow.java
@@ -5,11 +5,14 @@ import java.awt.*;
 
 public class MainWindow extends JFrame {
     private final Dimension minSz = new Dimension(600, 400);
+    private static final int  std = 8;
     public MainWindow(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setMinimumSize(minSz);
 
         GroupLayout gl = new GroupLayout(getContentPane());
         setLayout(gl);
+
+
     }
 }
